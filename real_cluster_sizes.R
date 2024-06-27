c5bb4cd6be44b3980d0369948ca886f766406f41
Gloria Buriticá <gloriaburit@gmail.com>
2024-06-27 12:09:19 +0200

    compute true cluster sizes

--- real_cluster_sizes.R
+++ real_cluster_sizes.R
@@ -9,26 +9,31 @@
 #### Boxplot of estimates of the cluster index of sums and the extremal index
 #### Comparison: alpha-cluster-based vs. maxima-cluster-based estimator
 #######################################################################
-res <- NULL
-for(i in 1:10000){
-  n <- 50000
-  E <- rexp(1)
-  St <- cumsum(c(rnorm(n) - 0.5))
-  s  <- sum(St > -E)
-  res <- c(res, s)
-  print(i)
+
+real_cluster_sizes <- function(N=10000){
+  res <- NULL
+  for(i in 1:N){
+    n <- 50000
+    E <- rexp(1)
+    St <- cumsum(c(rnorm(n) - 0.5))
+    s  <- sum(St > -E)
+    res <- c(res, s)
+    #print(i)
+  }
+  ### pi_j
+  theta <- mean(res==0)
+  ci    <- qnorm(0.975)*sd((res==0))/sqrt(length(res))
+  theta <- c(theta-ci,theta,theta+ci)
+  #print(theta)
+  for(j in 0:4){
+    ci  <- qnorm(0.975)*sd( (res==j) - (res ==(j+1) ) )/sqrt(length(res))
+    pij <- (mean(res==j) - mean(res==(j+1) )) 
+    pij <- c(pij- ci, pij,pij+ci) 
+    theta <- cbind(theta,pij)
+  }
+  colnames(theta) <- c('theta', 'pi0', 'pi1', 'pi2', 'pi3', 'pi4' )
+  rownames(theta) <- c( 'lbound', 'estimate' , 'ubound' )
+  as.data.frame(theta)  
 }
-
-### pi_j
-j <- 0
-ci  <- qnorm(0.975)*sd( (res==j) - (res ==(j+1) ) )/sqrt(length(res))
-pij <- (mean(res==j) - mean(res==(j+1) )) 
-pij <- c(pij- ci, pij,pij+ci) 
-print(pij)
-
-### Extremal index
-theta <- mean(res==0)
-ci    <- qnorm(0.975)*sd((res==0))/sqrt(length(res))
-theta <- c(theta-ci,theta,theta+ci)
-print(theta)
+#real_cluster_sizes(10)
 
